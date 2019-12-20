38361026daf0b2af9f4bc5802abaae7ef33e1570
Nolan Picini <nolanpic@gmail.com>
2019-12-20 09:21:49 -0800

    Added styled components and styles file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,32 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from 'styled-components';
+import { colors } from '../styles';
 
 export default function Header() {
+
+  const StyledHeader = styled.header`
+    nav {
+      display: flex;
+      background-color: ${colors.lightRed};
+      padding: 1rem 0;
+      border: 10px solid ${colors.darkRed};
+      
+      a {
+        padding-left: 2rem;
+        text-decoration: none;
+        color: ${colors.offWhite};
+      }
+    }
+  `;
+
   return (
-    <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+    <StyledHeader>
+      <h1>Rick &amp; Morty Fan Page</h1>
       <nav>
         <NavLink to="/">Home</NavLink>
         <NavLink to="/characters">Characters</NavLink>
       </nav>
-    </header>
+    </StyledHeader>
   );
 }
---
+++ src/styles.js
@@ -0,0 +1,8 @@
+
+export const colors = {
+    darkGreen: '#05200c',
+    lightGreen: 'rgb(111, 255, 139)',
+    darkRed: 'rgb(54, 46, 45)',
+    lightRed: '#5c4547',
+    offWhite: '#b9d0c1'
+};
