3ca2f02cb35ea033520b8b08d0d3a35b54b794fe
Nolan Picini <nolanpic@gmail.com>
2019-12-20 11:25:41 -0800

    Stretch: Added details view

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -1,7 +1,45 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import styled from 'styled-components';
 
-export default function CharacterDetails(props) {
+const StyledCharacterDetails = styled.section`
+    img {
+        float: right;
+    }
+`;
+
+export default function CharacterDetails() {
+
+    const { id } = useParams();
+
+    const [character, setCharacter] = useState(null);
+
+    useEffect(() => {
+        axios.get(`https://rickandmortyapi.com/api/character/${id}`)
+            .then(res => {
+                console.log('character', res);
+                setCharacter(res.data);
+            })
+            .catch(err => console.warn(err));
+    }, []);
+
     return (
-        <h2>Character Details</h2>
+        <StyledCharacterDetails>
+            {character && <>
+                <h2>{character.name}</h2>
+                <img src={character.image} alt={`Portrait of ${character.name}`} />
+                <ul>
+                    <li>Species: {character.species}</li>
+                    <li>Gender: {character.gender}</li>
+                    {character.type.length > 0 && 
+                    <li>Type: {character.type}</li>}
+                    <li>Status: {character.status}</li>
+                    <li>Origin: {character.origin.name}</li>
+                    <li>Location: {character.location.name}</li>
+                    <li>Appears in {character.episode.length} { character.episode.length > 1 ? 'episodes' : 'episode'}</li>
+                </ul>
+            </>}
+        </StyledCharacterDetails>
     );
 }
